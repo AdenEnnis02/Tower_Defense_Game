95673c7d60a212f9c50d3da939ce8a0ca5aeff81
Kpenguin03 <90807445+Kpenguin03@users.noreply.github.com>
2023-05-04 14:38:48 -0600

    DrawSquares

---
+++ MakeSquares.cs
@@ -0,0 +1,17 @@
+          
+
+using Raylib_cs;
+using System.Numerics;
+
+public class MakeSquares
+{
+    
+            Rectangle square1 = new Rectangle(100, 100, 50, 50);
+            Rectangle square2 = new Rectangle(200, 200, 50, 50);
+// The first parameter 100 represents the X-coordinate of the top-left corner of the rectangle.
+// The second parameter 100 represents the Y-coordinate of the top-left corner of the rectangle.
+// The third parameter 200 represents the width of the rectangle.
+// The fourth parameter 150 represents the height of the rectangle.
+            //Then to actually draw it add this in the game loop... Raylib.DrawRectangleRec(square1, Color.RED); or Raylib.DrawRectangleRec(square2, Color.BLUE);
+
+}
