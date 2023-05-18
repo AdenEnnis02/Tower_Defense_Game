c01fcbcf48c41d2d319c517359b87239669b588f
Slangfroid <107950770+Slangfroid@users.noreply.github.com>
2023-05-18 13:18:44 -0600

    updating projectile.cs

--- projectile.cs
+++ projectile.cs
@@ -1,14 +1,15 @@
-public class Projectile : MonoBehaviour {
+using raylib;
+public class Projectile {
     public float speed = 8.5f; // Speed of projectile.
     public float radius = 1f; // Collision radius.
     float radiusSq; // Radius squared; optimisation.
     Transform target; // Who we are homing at.
 
-    Vector3 currentPosition; // Store the current position we are at.
+    Vector2 currentPosition; // Store the current position we are at.
     float distanceTravelled; // Record the distance travelled.
 
     public float arcFactor = 0.5f; // Higher number means bigger arc.
-    Vector3 origin; // To store where the projectile first spawned.
+    Vector2 origin; // To store where the projectile first spawned.
 
     void OnEnable() {
         // Pre-compute the value. 
@@ -20,19 +21,21 @@ public class Projectile : MonoBehaviour {
         // If there is no target, destroy itself and end execution.
         if ( !target ) {
             Destroy(gameObject);
-            // Write your own code to spawn an explosion / splat effect.
+            // Write more code to spawn an explosion / splat effect.
+
+
             return; // Stops executing this function.
         }
 
         // Move ourselves towards the target at every frame.
-        Vector3 direction = target.position - currentPosition;
-        currentPosition += direction.normalized * speed * Time.deltaTime;
+        Vector2 direction = target.position - currentPosition;
+        currentPosition += direction.Normalize() * speed * Time.deltaTime;
         distanceTravelled += speed * Time.deltaTime; // Record the distance we are travelling.
 
         // Set our position to <currentPosition>, and add a height offset to it.
-        float totalDistance = Vector3.Distance(origin, target.position);
+        float totalDistance = Vector2.Distance(origin, target.position);
         float heightOffset = arcFactor * totalDistance * Mathf.Sin( distanceTravelled * Mathf.PI / totalDistance );
-        transform.position = currentPosition + new Vector3( 0, 0, heightOffset );
+        transform.position = currentPosition + new Vector2( 0, 0, heightOffset );
 
         // Destroy the projectile if it is close to the target.
         if ( direction.sqrMagnitude < radiusSq ) {
@@ -43,7 +46,7 @@ public class Projectile : MonoBehaviour {
     }
 
     // So that other scripts can use Projectile.Spawn to spawn a projectile.
-    public static Projectile Spawn(GameObject prefab, Vector3 position, Quaternion rotation, Transform target) {
+    public static Projectile Spawn(GameObject prefab, Vector2 position, Quaternion rotation, Transform target) {
         // Spawn a GameObject based on a prefab, and returns its Projectile component.
         GameObject go = Instantiate(prefab, position, rotation);
         Projectile p = go.GetComponent<Projectile>();
